bd39fbabf056a3a0d1ccd28f425aae955fb1fd12
Michael Núñez <mnunez@comscore.com>
2023-02-21 16:04:34 -0300

    running tests on server using docker

---
+++ django/tests/docker-tests/smoke.test.js
@@ -0,0 +1,23 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+  vus: 1, // 1 user looping for 1 minute
+  duration: '1m',
+
+  thresholds: {
+    http_req_duration: ['p(99)<500'], // 99% of requests must complete below .5s
+  },
+};
+
+let url = 'http://10.101.78.145:4004/';
+
+export default () => {
+  const resp = http.get(url);
+
+  check(resp, {
+    'received data successfully': (resp) => resp.json('message') !== '',
+  });
+
+  sleep(1);
+};
---
+++ fastapi/tests/docker-tests/sqlconnection.test.js
@@ -0,0 +1,19 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+let url = 'http://10.101.78.145:4002/users';
+
+export const options = {
+  stages: [
+    { duration: '2m', target: 20 },
+    { duration: '8m', target: 20 },
+  ],
+};
+
+export default function () {
+  const responses = http.batch([
+    ['GET', url, null],
+  ]);
+
+  sleep(1);
+}
